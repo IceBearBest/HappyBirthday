c8ac4d126487c211720d8e9a44e0a670f7efd95b
qlj <qlj699@gmail.com>
2022-01-15 20:31:36 -0800

    add cat-head gif hhhhh

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
-document.querySelector('#cat_xyl').onclick = function(){
+let myImage = document.querySelector('#cat-xyl');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cat.jpg') {
+      myImage.setAttribute('src', 'images/cat-head.gif');
+    } else {
+      myImage.setAttribute('src', 'images/cat.jpg');
+    }
     alert('肖宇梁2022暴富！！！')
 }
