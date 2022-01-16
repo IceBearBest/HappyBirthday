7e1981393ecf569c0c53b6e348023909128eacbc
qlj <qlj699@gmail.com>
2022-01-16 01:54:46 -0800

    add firework

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
+window.onload = function () {
+    var firework = JS_FIREWORKS.Fireworks({
+        id : 'fireworks-canvas',
+        hue : 120,
+        particleCount : 100,
+        delay : 0,
+        minDelay : 20,
+        maxDelay : 40,
+        boundaries : { // of respawn and target
+            top: 50,
+            bottom: 240,
+            left: 50,
+            right: 590
+        },
+        fireworkSpeed : 3,
+        fireworkAcceleration : 1.05,
+        particleFriction : .95,
+        particleGravity : 1.5
+    });
+    firework.start();
+};
 
 function generateWish(){
     addr = ['肖宇梁','小月亮','梁梁','梁仔','鱼粮','梁咪']
